740f65cba128daf315d856485bfa91e28c26bbb3
bibiduarte <bibiduarte@yahoo.com>
2024-10-19 22:42:11 -0500

    CRUD de Categoria

--- SistemaInventario.AccesoDatos/Data/ApplicationDbContext.cs
+++ SistemaInventario.AccesoDatos/Data/ApplicationDbContext.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace SistemaInventario.AccesoDatos.Data
@@ -16,6 +17,30 @@ namespace SistemaInventario.AccesoDatos.Data
         : base(options)
         { }
         public DbSet<Bodega> Bodegas { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+        /*
+        public DbSet<Marca> Marcas { get; set; }
+
+        public DbSet<Producto> Productos { get; set; }
+
+        public DbSet<UsuarioAplicacion> UsuarioAplicacion { get; set; }
+
+        public DbSet<BodegaProducto> BodegasProductos { get; set; }
+
+        public DbSet<Inventario> Inventarios { get; set; }
+
+        public DbSet<InventarioDetalle> InventarioDetalles { get; set; }
+
+        public DbSet<KardexInventario> KardexInventarios { get; set; }
+
+        public DbSet<Compania> Companias { get; set; }
+
+        public DbSet<CarroCompra> CarroCompras { get; set; }
+
+        public DbSet<Orden> Ordenes { get; set; }
+
+        public DbSet<OrdenDetalle> OrdenDetalles { get; set; }
+        */
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
